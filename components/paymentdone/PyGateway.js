ae7ec172270e1e8963712b129dc828c6250930b7
Abhishek <a1abhishek2016@gmail.com>
2023-09-19 11:15:42 +0530

    misc

---
+++ components/paymentdone/PyGateway.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PyGateway = () => {
+    return (
+        <View>
+            <Text>PyGateway</Text>
+        </View>
+    )
+}
+
+export default PyGateway
