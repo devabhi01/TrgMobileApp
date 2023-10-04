4a17d5e0ea861209fa8b7e324b9c6aa6037d0bf9
Ravi Mishra <ravim70655@gmail.com>
2023-10-04 16:45:25 +0530

    Added search functionality on homepage

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -6,13 +6,13 @@ import {
   ScrollView,
   StyleSheet,
   TouchableOpacity,
-  Image
+  Image,
+  // PermissionsAndroid
 } from 'react-native';
-import {Searchbar} from 'react-native-paper';
+import { Searchbar, Avatar } from 'react-native-paper';
 // import Carousel from 'react-native-snap-carousel';
-import {windowWidth} from '../utils/Dimensions';
-import {Avatar} from 'react-native-paper';
-import {colors} from '../constants';
+import { windowWidth } from '../utils/Dimensions';
+import { colors } from '../constants';
 
 // Images here
 import ManSVG from '../assets/img/Man.svg';
@@ -22,20 +22,51 @@ import { sliderData } from '../model/Data';
 import BannerSlider from '../components/BannerSlider';
 import { useUserContext } from '../utils/userContext';
 import Geolocation from '@react-native-community/geolocation';
-import {PermissionsAndroid} from 'react-native';
 
+// Course details
+const courses = [
+  { name: "cbse", screen: "cbse", img: require("../assets/img/cbse.png") },
+  { name: "icse", screen: "icse", img: require("../assets/img/icse.png") },
+  { name: "ssc", screen: "ssc", img: require("../assets/img/ssc.png") },
+  { name: "cuet", screen: "cuet", img: require("../assets/img/cuet.png") },
+  { name: "clat", screen: "clat", img: require("../assets/img/clat.png") },
+  { name: "dsssb", screen: "dsssb", img: require("../assets/img/dsssb.png") },
+  { name: "police", screen: "dsssb", img: require("../assets/img/police.png") },
+  { name: "others", screen: "dsssb", img: require("../assets/img/other.png") },
+  { name: "quiz", screen: "quiz", img: require("../assets/img/Quiz.png") },
+  { name: "test", screen: "test", img: require("../assets/img/test.png") },
+]
 
 const HomeScreen = props => {
   // asking notifications permission
-  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+  // PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
   // getting current geolocation
   Geolocation.getCurrentPosition(info => console.log(info));
 
-  const {user} = useUserContext()
-  
+  // user context for fetching details
+  const { user } = useUserContext()
+
   const [searchQuery, setSearchQuery] = useState('');
 
-  const onChangeSearch = query => setSearchQuery(query);
+  // filtered course state
+  const [filteredCourses, setFilteredCourses] = useState(courses)
+
+  const onChangeSearch = query => {
+    setSearchQuery(query)
+    // Convert the query to lowercase for case-insensitive search
+    const lowercaseQuery = query.toLowerCase();
+
+    // Use Array.prototype.filter() to filter objects based on the query
+    const filteredCourses = courses.filter(item => {
+      // Check if any property of the item contains the query string (case-insensitive)
+      return Object.values(item).some(value =>
+        typeof value === 'string' && value.toLowerCase().includes(lowercaseQuery)
+      );
+    });
+
+    setFilteredCourses(filteredCourses);
+
+  };
 
   const renderBanner = ({ item, index }) => {
     return <BannerSlider data={item} />;
@@ -48,7 +79,7 @@ const HomeScreen = props => {
           <View style={styles.topbar}>
             <View style={{ marginLeft: 20 }}>
               <Text style={{ color: '#eee', fontSize: 20, color: '#dc3545' }}>
-                Hello,
+                Hello, {user.name}
               </Text>
               <Text
                 style={{
@@ -93,6 +124,7 @@ const HomeScreen = props => {
                 borderRadius: 10,
                 backgroundColor: '#f5f5f5',
               }}
+              onTouchCancel={()=>{setSearchQuery('')}}
             />
           </View>
 
@@ -144,59 +176,17 @@ const HomeScreen = props => {
               justifyContent: 'space-around',
               marginBottom: 50,
             }}>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('cbse')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/cbse.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('icse')}
-              style={styles.ex_categories}>
-              <Image
-                resizeMethod="auto"
-                source={require('../assets/img/icse.png')}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('ssc')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/ssc.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('cuet')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/cuet.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('clat')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/clat.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('dsssb')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/dsssb.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('dsssb')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/police.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('dsssb')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/other.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('quiz')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/Quiz.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => props.navigation.navigate('test')}
-              style={styles.ex_categories}>
-              <Image source={require('../assets/img/test.png')} />
-            </TouchableOpacity>
+
+            {filteredCourses.map((course, index) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => props.navigation.navigate(course.screen)}
+                  style={styles.ex_categories}
+                  key={index}>
+                  <Image source={course.img} />
+                </TouchableOpacity>)
+            })}
+
           </View>
         </View>
       </ScrollView>
