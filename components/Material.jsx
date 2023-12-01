745d93682683fbe402002cb178b83ebabe096099
Abhishek <a1abhishek2016@gmail.com>
2023-12-01 22:00:56 +0530

    changes material done

--- components/Material.jsx
+++ components/Material.jsx
@@ -1,12 +1,67 @@
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import { colors } from '../constants';
 import React from 'react';
 import MoreOptBtn from './courses/template/MoreOptBtn';
+import {useNavigation} from '@react-navigation/native';
+import {courseList, classData,subjectListForbelow11,subjectListForafter10} from '../model';
+import CustomDropdown from './courses/template/CustomDropdown';
 
-const Material = (props) => {
+const Material = props => {
+  const navigation = useNavigation();
+
+  const material = [
+    {
+      materialName: 'Revision Notes',
+    },
+    {
+      materialName: 'Support Material',
+    },
+    {
+      materialName: 'Syllabus',
+    },
+    {
+      materialName: 'Assignments',
+    },
+    {
+      materialName: 'NCERT Books',
+    },
+    {
+      materialName: 'NCERT Solutions',
+    },
+    {
+      materialName: 'Previous Year Question Paper',
+    },
+  ];
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
       <ScrollView>
         <View style={styles.conatiner}>
+          <CustomDropdown 
+          initialValue={material}
+          innerList={'materialName'}
+          displayName={'Material'}
+          menuHeight={200}
+          />
+          <CustomDropdown 
+          initialValue={courseList}
+          innerList={'courseName'}
+          displayName={'Course'}
+          
+          />
+          <CustomDropdown 
+          initialValue={classData}
+          innerList={'class'}
+          displayName={'Course'}
+          menuHeight={200}
+          />
+          <CustomDropdown 
+          initialValue={subjectListForbelow11}
+          innerList={'subjectName'}
+          displayName={'Subject'}
+          menuHeight={200}
+          />
+
 
           {/* <View style={styles.btnCont}>
             <MoreOptBtn
@@ -66,9 +121,11 @@ const Material = (props) => {
               backgroundColor={'#dc3545'}
             />
           </View> */}
-          
         </View>
       </ScrollView>
+      <TouchableOpacity style={styles.buyBtn}>
+        <Text style={{fontSize: 18, color: '#fff'}}>Submit</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -78,8 +135,20 @@ export default Material;
 const styles = StyleSheet.create({
   conatiner: {
     flex: 1,
+    margin: 20,
   },
   btnCont: {
-    margin:20,
+    margin: 20,
+  },
+  buyBtn: {
+    width: '100%',
+    height: 50,
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
   },
 });
--- model/index.js
+++ model/index.js
@@ -1,2 +1,2 @@
-import {courseList, classData} from './CourseData';
-export {courseList, classData};
+import {courseList, classData, subjectListForbelow11,subjectListForafter10} from './CourseData';
+export {courseList, classData ,subjectListForbelow11,subjectListForafter10};
